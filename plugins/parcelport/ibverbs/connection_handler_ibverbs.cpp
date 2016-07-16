b6cb4fa12db0ed7684be5012e6c413ada0b247a6
Agustin K-ballo Berge <k@fusionfenix.com>
2016-07-16 09:23:09 -0300

    Replace boost::posix_time with chrono where possible

--- plugins/parcelport/ibverbs/connection_handler_ibverbs.cpp
+++ plugins/parcelport/ibverbs/connection_handler_ibverbs.cpp
@@ -20,6 +20,7 @@
 
 #include <boost/atomic.hpp>
 #include <boost/assign/std/vector.hpp>
+#include <boost/chrono/chrono.hpp>
 #include <boost/io/ios_state.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/ip/host_name.hpp>
@@ -317,9 +318,8 @@ namespace hpx { namespace parcelset { namespace policies { namespace ibverbs
                         "connection_handler(ibverbs)::create_connection");
                 }
                 else {
-                    boost::this_thread::sleep(boost::get_system_time() +
-                        boost::posix_time::milliseconds(
-                            HPX_NETWORK_RETRIES_SLEEP));
+                    boost::this_thread::sleep_for(
+                        boost::chrono::milliseconds(HPX_NETWORK_RETRIES_SLEEP));
                 }
             }
             catch (boost::system::system_error const& e) {
